db60917ef369653cc2fd545c3a621fc950d5c12a
creative_parasite <nyawijuanki.gmail.com>
2024-07-15 16:31:29 +0300

    Complete

--- Streamlit/home.py
+++ Streamlit/home.py
@@ -21,14 +21,14 @@ def main():
         st.subheader('Sepsis Input Features')
         col1, col2, = st.columns(2)
         with col1:
-            prg = st.number_input('PRG', min_value=0, step=1)
-            pl = st.number_input('PL', min_value=0, step=1)
-            pr = st.number_input('PR', min_value=0, step=1)
-            sk = st.number_input('SK', min_value=0, step=1)
+            prg = st.number_input('Plasma glucose levels', min_value=0, step=1)
+            pl = st.number_input('Blood work results 1', min_value=0, step=1)
+            pr = st.number_input('Blood pressure', min_value=0, step=1)
+            sk = st.number_input('Blood work results 2', min_value=0, step=1)
         with col2:
-            ts = st.number_input('TS', min_value=0, step=1)
-            m11 = st.number_input('M11', min_value=0, step=1)
-            bd2 = st.number_input('BD2', min_value=0, step=1)
+            ts = st.number_input('Blood work results 3', min_value=0, step=1)
+            m11 = st.number_input('Body mass index', min_value=0, step=1)
+            bd2 = st.number_input('Blood work results 4', min_value=0, step=1)
             age = st.number_input('Age', min_value=21, step=1)
             insurance = st.radio('Insurance', options=['Yes', 'No'])
             
