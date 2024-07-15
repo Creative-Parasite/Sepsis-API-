157db866b0513ea22da12dcb3c2765f6fbbf60fb
creative_parasite <nyawijuanki.gmail.com>
2024-07-15 16:27:37 +0300

    Update frontend app

--- Streamlit/home.py
+++ Streamlit/home.py
@@ -1,9 +1,8 @@
 import streamlit as st
 import pandas as pd 
-import numpy as np
-import joblib
 import requests
-import json
+
+backend_url = "http://127.0.0.1:8000"
 
 st.set_page_config(
     page_title= 'SEPSIS API',
@@ -11,68 +10,50 @@ st.set_page_config(
     layout='wide'
 )
 
-## FAST API
-response = requests.get()
-
-models = {
-    'random_forest': 'random_forest',
-    'gradient_boost': 'gradient',
-}
-
 def main():
     st.title('SEPSIS API')
-    
-    st.write("""
-         Sepsis also known as **Blood poisoning**
+    st.markdown("""
+         Sepsis also known as **Blood poisoning** -
          An integration of API for detecting Sepsis on patients in the ICU. 
          """
           )
-
-    ## Create a select box 
-    with st.sidebar:
-        selection = st.selectbox("Please select your models", list(models.key()))
+    with st.form('input-feature'):
+        st.subheader('Sepsis Input Features')
+        col1, col2, = st.columns(2)
+        with col1:
+            prg = st.number_input('PRG', min_value=0, step=1)
+            pl = st.number_input('PL', min_value=0, step=1)
+            pr = st.number_input('PR', min_value=0, step=1)
+            sk = st.number_input('SK', min_value=0, step=1)
+        with col2:
+            ts = st.number_input('TS', min_value=0, step=1)
+            m11 = st.number_input('M11', min_value=0, step=1)
+            bd2 = st.number_input('BD2', min_value=0, step=1)
+            age = st.number_input('Age', min_value=21, step=1)
+            insurance = st.radio('Insurance', options=['Yes', 'No'])
+            
+        submit_button = st.form_submit_button('Predict')
         
+        if submit_button:
+            input_data = {
+                'prg': prg,
+                'pl': pl,
+                'pr': pr,
+                'sk': sk,
+                'ts': ts,
+                'm11': m11,
+                'bd2': bd2,
+                'age': age,
+                'insurance': insurance
+            }
+            
+            response = requests.post(f"{backend_url}/predict_sepsis", json=input_data)
+            
+            if response.status_code == 200:
+                prediction = response.json()['prediction']
+                st.success(f'The patient tested {prediction} for sepsis')
+            else:
+                st.error(f'Error: {response.json()["detail"]}')
         
-    ## get models file name based on selection
-    model_name = models[selection]
-    
-    
-    ## Features 
-    prg = st.number_input("Plasma glucose level")
-    pl = st.number_input("Blood work results 1")
-    pr = st.number_input("Blood pressure")
-    sk = st.number_input("Blood work results 2")
-    ts = st.number_input("Blood work results 3")
-    m11 = st.number_input("Body mass index", min_value=0, step=1)
-    bd2 = st.number_input("Blood work results 4")
-    age = st.number_input("What is the patient's age?", min_value=0, max_value= 70, step=1)
-    insurance = st.radio('Insurance', options=['Yes', 'No'])
-    
-    if st.button('Predict'):
-        ## Prepare feature data as JSON payload
-        feature_data= {
-            'prg': prg,
-            'pl': pl,
-            'pr': pr,
-           'sk': sk,
-            'ts': ts,
-           'm11': m11,
-            'bd2': bd2,
-            'Age': age,
-            'Insurance': insurance
-        }
-        
-        feature_data = [prg, pl, pr, sk, ts, m11,bd2, age, insurance]
-        
-        ## Json serialization
-        # Call FastAPI endpoint and get prediction result
-        headers = {'Content-Type': 'application/json'}
-        response = requests.post(response + f"/{model_name}", json=feature_data)
-
-        # Display prediction result
-        st.write(f"Prediction: {response.json()}")
-    
-if __name__ == "__main__":
-    main() 
-        
-
+if __name__ == '__main__':
+    main()
